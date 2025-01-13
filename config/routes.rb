3158f8600af972e0644fdd930c3906a398f76766
andrea grah <andreagrah@yahoo.co.uk>
2025-01-13 19:22:05 +0000

    Add navigation links and back buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'categories#index'
   resources :categories, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
